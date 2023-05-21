05306afb1956b4b2789672fcff170c74e6e2b78e
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-05-21 01:00:49 +0000

    adding templates

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -6,12 +6,25 @@ from .models import Tweet
 # Create your views here.
 def home_view(request,*args, **kwargs):
     print(args, kwargs)
-    return HttpResponse('<h1>Hello world</h1>')
+    #return HttpResponse('<h1>Hello world</h1>')
+    return render(request, "pages/home.html", context={}, status=200)    # render is used for templates
 
 def tweets_detail_view(request, tweet_id, *args, **kwargs):
+    """ 
+    Coverting the dynamic view into REST View(Jason Format)
+    """
     print(args,kwargs)
+
+    # json data
+    data = {
+        "id" : tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id = tweet_id)
+        data['content'] = obj.content  # if id exist add content to json data
     except:
-        raise Http404
-    return HttpResponse(f'<h1>Hello {tweet_id} - {obj.content}</h1>')
+        data['message'] = "Not Found"
+        status = 404
+    
+    return JsonResponse(data=data, status=status)
