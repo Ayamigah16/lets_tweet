a43804d3a68abfbd803de0397eafee9ac204e96a
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-05-21 01:45:08 +0000

    tweet_list

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tweets.views import home_view,tweets_detail_view
+from tweets.views import home_view,tweets_detail_view, tweet_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('tweets/', tweet_list),
     path('tweets/<int:tweet_id>', tweets_detail_view),
 
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -9,6 +9,18 @@ def home_view(request,*args, **kwargs):
     #return HttpResponse('<h1>Hello world</h1>')
     return render(request, "pages/home.html", context={}, status=200)    # render is used for templates
 
+def tweet_list(request, *args, **kwargs):
+    """ 
+    REST API VIEW
+    """
+    
+    qs = Tweet.objects.all()
+    tweets_list = [{"id":x.id, "content":x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
 def tweets_detail_view(request, tweet_id, *args, **kwargs):
     """ 
     Coverting the dynamic view into REST View(Jason Format)
