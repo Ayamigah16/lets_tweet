69af1256cfd020790efdb9c2f6ef8dc16f8a115a
Abraham Ayamigah <122880578+Ayamigah16@users.noreply.github.com>
2023-05-23 15:34:09 +0000

    adding like field

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 
-from .models import Tweet
+from .models import Tweet, TweetLike
 # Register your models here.
+
+class TweetLikeAdmin(admin.TabularInline):     
+    model = TweetLike
 
 class TweetAdmin(admin.ModelAdmin):
     class Meta:
         model = Tweet
 
+    inlines = [TweetLikeAdmin]
     search_fields = ['content','user__username','user__email']
     list_display = ['__str__','user']
 
--- tweets/models.py
+++ tweets/models.py
@@ -6,11 +6,17 @@ User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User,related_name='tweet_user', blank=True,through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
-
+    timestamp = models.DateTimeField(auto_now_add=True)
     # TODO  -> More tweet features to add
 
     class Meta:
